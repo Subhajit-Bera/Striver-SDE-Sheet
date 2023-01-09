3ecea951d6735ff8207f8ad8daa8f0307470654d
Subhajit <subhabera211@gmail.com>
2023-01-09 23:13:51 +0530

    Create InorderTraversalIterative.java

---
+++ BinaryTree/src/InorderTraversalIterative.java
@@ -0,0 +1,18 @@
+//Symmetric Tree
+
+public class InorderTraversalIterative {
+    //TC:0(n)  SC:0(n) In the worst case (skewed tree), space complexity can be O(N)
+    public boolean isSymmetric(TreeNode root) {
+        return root==null || isSymmetricHelp(root.left,root.right);
+    }
+    private boolean isSymmetricHelp(TreeNode left ,TreeNode right){
+        if(left==null || right==null){
+            return left==right;
+        }
+        if(left.val !=right.val){
+            return false;
+        }
+
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right , right.left );
+    }
+}
